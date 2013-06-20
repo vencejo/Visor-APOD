fae8b451c2cbd0b02a7c12abb6d114de46d76703
vencejo <apussapus@gmail.com>
2013-06-20 20:29:13 +0200

    borrado el archivo constantes que ya es inservible

--- GUI/constantes.py
+++
@@ -1,4 +0,0 @@
-
-rutaImagenesMedianas = '/home/dj/Escritorio/thumbs/medianas'
-rutaImagenesGrandes = '/home/dj/Escritorio/'
-rutaDeScrapy = '/home/dj/Escritorio/Proyecto final curso/Pruebas/prueba_scrapy_mysqul_gui/APOD_scrapy'
